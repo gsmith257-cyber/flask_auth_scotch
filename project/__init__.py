2a144ca31df293ee59699d7b997bb9748060eeb4
S1n1st3r <S1n1st3r@gmail.com>
2022-10-26 15:04:48 -0400

    Added docker support

--- project/__init__.py
+++ project/__init__.py
@@ -38,4 +38,7 @@ def create_app():
     with app.app_context():
         db.create_all()
 
+    port = int(os.environ.get('PORT', 80))
+    app.run(debug=False, host='0.0.0.0', port=port)
+
     return app
