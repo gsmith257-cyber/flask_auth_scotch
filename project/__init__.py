ded1b45895fd73d5ec0e62bce3524dfadd4d0a80
gsmith257-cyber <55564824+gsmith257-cyber@users.noreply.github.com>
2022-10-26 17:32:05 -0400

    bug fix

--- project/__init__.py
+++ project/__init__.py
@@ -38,9 +38,6 @@ def create_app():
     with app.app_context():
         db.create_all()
 
-    port = int(os.environ.get('PORT', 80))
-    app.run(debug=False, host='0.0.0.0', port=port)
-
     return app
 
 if __name__ == "__main__":
