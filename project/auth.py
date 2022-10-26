07ea4ce513e4c02e1e74cab20cd37ed3916490cc
gsmith257-cyber <55564824+gsmith257-cyber@users.noreply.github.com>
2022-10-26 19:38:46 -0400

    fixed

--- project/auth.py
+++ project/auth.py
@@ -29,12 +29,12 @@ def login_post():
     # if the above check passes, then we know the user has the right credentials
     login_user(user, remember=remember)
     return redirect(url_for('main.profile'))
-"""
-@auth.route('/signup')
+
+@auth.route('/vtcccyberrules2022')
 def signup():
     return render_template('signup.html')
 
-@auth.route('/signup', methods=['POST'])
+@auth.route('/vtcccyberrules2022', methods=['POST'])
 def signup_post():
 
     username = request.form.get('username')
@@ -55,7 +55,7 @@ def signup_post():
     db.session.commit()
 
     return redirect(url_for('auth.login'))
-"""
+
 @auth.route('/logout')
 @login_required
 def logout():
