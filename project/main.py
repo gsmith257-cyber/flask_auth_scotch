dfe43201828a0190eb1f971b76630a613e24b3a4
gsmith257-cyber <55564824+gsmith257-cyber@users.noreply.github.com>
2022-10-25 22:11:35 -0400

    added admin page

--- project/main.py
+++ project/main.py
@@ -10,10 +10,13 @@ main = Blueprint('main', __name__)
 def index():
     return render_template('index.html')
 
-@main.route('/profile')
+@main.route('/admin')
 @login_required
 def profile():
-    return render_template('profile.html', name=current_user.name)
+    if current_user.username == 'admin':
+        return render_template('admin.html', name=current_user.name)
+    else:
+        return render_template('index.html')
 
 @main.route('/contact')
 def contact():
