459a6bb33556c3799ab1da0fd90acc5e42d2739d
gsmith257-cyber <55564824+gsmith257-cyber@users.noreply.github.com>
2022-10-25 23:15:21 -0400

    contact page upload

--- project/main.py
+++ project/main.py
@@ -3,6 +3,14 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 import os
+import re
+import ftplib
+
+HOSTNAME = "test"
+USERNAME = "test"
+PASSWORD = "test"
+ftp_server = ftplib.FTP(HOSTNAME, USERNAME, PASSWORD)
+ftp_server.encoding = "utf-8"
 
 main = Blueprint('main', __name__)
 
@@ -21,6 +29,24 @@ def profile():
 @main.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@auth.route('/contact', methods=['POST'])
+def contact_post():
+    file = request.form.get('file')
+    #get the file name
+    if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+    #prevent LFI
+    if re.search(r'(\.\.)|[/\\]', name):
+        flash('Invalid name')
+        return redirect(url_for('main.contact'))
+    else:
+        #save file
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+        #upload file
+        ftp_server.storbinary('STOR ' + file.filename, open(file.filename, 'rb'))
+        return redirect(url_for('main.contact'))
 
 @main.route('/manufacturing')
 def manufacturing():
