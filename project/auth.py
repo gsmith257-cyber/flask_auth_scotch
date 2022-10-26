f1cca13264c75a0be3e7bfabd65ec6af6a7c2179
gsmith257-cyber <55564824+gsmith257-cyber@users.noreply.github.com>
2022-10-25 21:02:09 -0400

    updated email to username

--- project/auth.py
+++ project/auth.py
@@ -37,18 +37,18 @@ def signup():
 @auth.route('/signup', methods=['POST'])
 def signup_post():
 
-    email = request.form.get('email')
+    username = request.form.get('username')
     name = request.form.get('name')
     password = request.form.get('password')
 
-    user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
+    user = User.query.filter_by(email=username).first() # if this returns a user, then the email already exists in database
 
     if user: # if a user is found, we want to redirect back to signup page so user can try again  
-        flash('Email address already exists')
+        flash('Username already exists')
         return redirect(url_for('auth.signup'))
 
     # create new user with the form data. Hash the password so plaintext version isn't saved.
-    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+    new_user = User(email=username, name=name, password=generate_password_hash(password, method='sha256'))
 
     # add the new user to the database
     db.session.add(new_user)
