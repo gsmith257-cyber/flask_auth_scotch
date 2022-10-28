a384b861afb00c1914b934c7d9c44e935496b7cc
gsmith257-cyber <55564824+gsmith257-cyber@users.noreply.github.com>
2022-10-28 12:20:57 -0400

    fix

--- project/main.py
+++ project/main.py
@@ -1,7 +1,7 @@
 # main.py
 
 import imaplib
-from flask import Blueprint, render_template, request, redirect, url_for, flash, send_file
+from flask import Blueprint, render_template, request, redirect, url_for, flash, send_file, Flask
 from flask_login import login_required, current_user
 from werkzeug.utils import secure_filename
 import mysql.connector
@@ -30,11 +30,12 @@ REMOTE_SQL_PASS = "password"
 
 main = Blueprint('main', __name__)
 
-main.config['MAIL_SERVER']='10.0.40.73'
-main.config['MAIL_PORT'] = 25
-main.config['MAIL_USERNAME'] = 'admin@sunpartners.local'
-main.config['MAIL_PASSWORD'] = 'Blueteam2022'
-mail = Mail(main)
+app2 = Flask(__name__)
+app2.config['MAIL_SERVER']='10.0.40.73'
+app2.config['MAIL_PORT'] = 25
+app2.config['MAIL_USERNAME'] = 'admin@sunpartners.local'
+app2.config['MAIL_PASSWORD'] = 'Blueteam2022'
+mail = Mail(app2)
 
 @main.route('/')
 def index():
